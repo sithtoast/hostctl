d125fee37861f64a33fab4c7d11524b8961b5876
William Holt <sithtoast@gmail.com>
2026-03-29 19:27:57 -0400

    mix bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hostctl.MixProject do
   def project do
     [
       app: :hostctl,
-      version: "0.7.1",
+      version: "0.7.2",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
