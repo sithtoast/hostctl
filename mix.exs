93de16e5bb4caf9762e3cc90d13d9ebc5386bd3e
William Holt <sithtoast@gmail.com>
2026-03-30 15:17:30 -0400

    interactive installer + help

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hostctl.MixProject do
   def project do
     [
       app: :hostctl,
-      version: "0.9.3",
+      version: "0.9.4",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
