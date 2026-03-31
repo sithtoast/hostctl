8c39f5b067dc51f23643a06f338c34bd726800e1
William Holt <sithtoast@gmail.com>
2026-03-31 18:01:46 -0400

    ftp uploads outside of public are really fixed meow

--- lib/hostctl/ftp_server.ex
+++ lib/hostctl/ftp_server.ex
@@ -151,8 +151,7 @@ defmodule Hostctl.FtpServer do
     content = """
     local_root=#{home_dir}
     write_enable=YES
-    anon_other_write_enable=YES
-    anon_mkdir_write_enable=YES
+    virtual_use_local_privs=YES
     """
 
     with :ok <- escaped_mkdir_p(dir),
